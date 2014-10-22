bfe65597764536f79562f6bf8c9d0893c4474e69
pulsation <psamlong@laposte.net>
2014-10-22 17:56:21 +0200

    JS cleanup

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 #SpringMVC+Angular Example
 
-This is a very simple example application that combines Spring MVC and AngularJS using [webjars](http://www.webjars.org/). It also demonstrates the possibility of rendering [Thymeleaf](http://www.thymeleaf.org/) template partials through Angular.
+This is a very simple example application that combines Spring MVC and AngularJS using [webjars](http://www.webjars.org/). It also demonstrates the possibility of rendering [Thymeleaf](http://www.thymeleaf.org/) partials through Angular. *./gradlew run* to launch it.
--- src/main/webapp/resources/js/calculate.js
+++ src/main/webapp/resources/js/calculate.js
@@ -2,42 +2,18 @@ console.log("Javascript correctly included");
 
 angular.module('calculate', ['ui.bootstrap', 'ngSanitize', 'rx', 'ngCookies'])
 
-.factory('Auth', ['$cookieStore', '$http', function ($cookieStore, $http) {
-//    $http.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
-console.log('headers set');
-// wemadeyoulook.at/en/blog/implementing-basic-http-authentication-http-requests-angular/
-/*    $http.defaults.headers.common['Authorization'] = 'Basic ' + $cookieStore.get('authdata');
-    console.log($http.defaults.headers.common['Authorization']);*/
-
-    return {
-        setCredentials: function (username, password) {
-/*        console.log("Encoding");
-        var original = username + ':' + password;
-            var encoded = CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(username + ':' + password));
-            console.log("Encoded:");
-            console.log(encoded);
-            $http.defaults.headers.common.Authorization = 'Basic ' + encoded;
-
-            $cookieStore.put('authdata', encoded);*/
-        }
-    }
-}])
-
-.controller('CalculateCtrl',  function ($scope, $http, observeOnScope, Auth) {
+.controller('CalculateCtrl',  function ($scope, $http, observeOnScope) {
     console.log("Entered controller");
 
     $scope.operation = {};
 
     var httpRequest = function (newVal) {
-//        console.log("setting credentials");
-//        Auth.setCredentials('user', 'password');
-//        console.log("Credentials set");
         return $http({
             method: 'POST',
             url: '/partials/square',
             data: { "x": newVal }
         });
-    }
+    };
 
     // Create an observable
     var obs1 =
