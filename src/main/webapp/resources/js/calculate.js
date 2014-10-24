a6a3e4bda59f1ff06a31f70574b0a36d10cbe5de
pulsation <psamlong@laposte.net>
2014-10-24 11:03:14 +0200

    POST call from ui

--- src/main/webapp/resources/js/calculate.js
+++ src/main/webapp/resources/js/calculate.js
@@ -2,18 +2,29 @@ console.log("Javascript correctly included");
 
 angular.module('calculate', ['ui.bootstrap', 'ngSanitize', 'rx', 'ngCookies'])
 
-.controller('CalculateCtrl',  function ($scope, $http, observeOnScope) {
+.controller('CalculateCtrl',  function ($scope, $http, observeOnScope, $compile) {
     console.log("Entered controller");
 
     $scope.operation = {};
 
-    var httpRequest = function (newVal) {
+    var httpSquarePartial = function (newVal) {
         return $http({
             method: 'POST',
             url: '/partials/square',
             data: { "x": newVal }
         });
     };
+
+    var httpSaveResult = function (operation, value) {
+        return $http({
+            method: 'POST',
+            url: '/data/calculate-results/',
+            data: {
+                "operation" : operation,
+                "value"     : value
+            }
+        });
+    }
 
     // Create an observable
     var obs1 =
@@ -23,7 +34,7 @@ angular.module('calculate', ['ui.bootstrap', 'ngSanitize', 'rx', 'ngCookies'])
     .throttle(300)
     // Take async requests in the right order
     .flatMapLatest(function(operand) {
-        return (httpRequest(operand.newValue));
+        return (httpSquarePartial(operand.newValue));
     })
     // Don't terminate stream on failure
 //    .retry()
@@ -32,10 +43,26 @@ angular.module('calculate', ['ui.bootstrap', 'ngSanitize', 'rx', 'ngCookies'])
     // Subscribe to observer stream
     obs1.subscribe(function success(response) {
         // Replace HTML code with data content
-        $scope.operation.result = response.data;
+        $scope.operation.resultPartial = response.data;
     }, function failure(data) {
         // Handle error
-        console.log("Error:");
+        console.log("Error reading data:");
+        console.log(data);
+    });
+
+    var obs2 = $scope.$createObservableFunction('save')
+    .map(function (operand) {
+        return { operation: operand + "^2", value: operand * operand };
+    })
+    .flatMap(function (operationResult) {
+        return httpSaveResult(operationResult.operation, operationResult.value );
+    });
+
+    obs2.subscribe(function success(response) {
+        $scope.operation.saveStatus = true;
+    }, function failure(data) {
+        $scope.operation.saveStatus = false;
+        console.log("Error saving data:");
         console.log(data);
     });
 
