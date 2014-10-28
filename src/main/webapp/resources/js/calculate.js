e44c3ba66655da67719c13dd03e03e8a17b51c54
pulsation <psamlong@laposte.net>
2014-10-28 17:10:33 +0100

    Move observable chains to a separate service

--- src/main/webapp/resources/js/calculate.js
+++ src/main/webapp/resources/js/calculate.js
@@ -1,74 +1,132 @@
 angular.module('calculate', ['ui.bootstrap', 'ngSanitize', 'ngResource', 'rx', 'ngAnimate', 'ui.grid'])
 
+/**
+ * REST resource for operation results
+ */
+
 .factory('OperationResult', ['$resource', function ($resource) {
     return $resource('/data/calculate-results/');
 }])
 
-.controller('CalculateCtrl',  function ($scope, $http, observeOnScope, $timeout, OperationResult) {
+.service('observableChains', function ($http, OperationResult) {
 
-    $scope.operation = {};
-
-    var httpSquarePartial = function (newVal) {
+    // Load template partial
+    var httpSquarePartial = function (x) {
         return $http({
             method: 'POST',
             url: '/partials/square',
-            data: { "x": newVal }
+            data: { "x": x }
         });
     };
 
-    // Create an observable
-    var updatePartialObservable =
-    // Watch scope variable named "operation.operand"
-    $scope.$toObservable('operation.operand')
-    // Throttle stream in order to avoid too much ajax requests
-    .throttle(300)
-    // Take async requests in the right order
-    .flatMapLatest(function(operand) {
-        return (httpSquarePartial(operand.newValue));
-    })
-    // Don't terminate stream on failure
-//    .retry()
-    ;
+    return {
+
+        /**
+         * Throttles and returns an http request promise loading
+         * the template partial
+         */
+        updatePartialObservableFrom: function (originalObservable) {
+
+            return originalObservable
+
+            // Throttle stream in order to avoid too much ajax requests
+            .throttle(300)
+
+            // Take async requests in the right order
+            .flatMapLatest(function(operand) {
+                return (httpSquarePartial(operand.newValue));
+            });
+        },
+
+        /**
+         * Returns an http request promise saving an OperationResult resource
+         */
+        saveResultObservableFrom: function (originalObservable) {
+
+            return originalObservable
+
+            // Map operand to something readable
+            .map(function (operand) {
+                var result = new OperationResult();
+
+                result.operation = operand + "^2";
+                result.value = operand * operand;
+                return result;
+            })
+
+            .flatMap(function (operationResult) {
+                return operationResult.$save();
+            });
+        },
+
+        /**
+         * Returns an http request that loads history
+         */
+        loadHistoryObservableFrom: function (originalObservable) {
+
+            return originalObservable
+
+            // Load history as soon as the application is launched
+            .startWith(null)
+
+            .flatMap(function () {
+                var resultHistory = new OperationResult();
+                return resultHistory.$get();
+            });
+        }
+    };
+})
+
+.controller('CalculateCtrl',  function ($scope, $http, observeOnScope, $timeout, OperationResult, observableChains) {
+
+    /**
+     * Display the proper alert once the entry has been saved
+     */
+    var updateSaveStatus = function (success) {
+        if (success) {
+            $scope.operation.saveStatus = true;
+                if ($scope.alertTimeout !== null) {
+                    $timeout.cancel($scope.alertTimeout);
+                }
+                $scope.alertTimeout = $timeout(function () {
+                    $scope.operation.saveStatus = null;
+                }, 1500
+            );
+        } else {
+            $scope.operation.saveStatus = false;
+        }
+    }
+
+    // Initialize scope variables
+    $scope.operation    = {};
+    $scope.alertTimeout = null;
+
+    // Create an observable from the scope variable named "operation.operand"
+    var updatePartialObservable = observableChains.updatePartialObservableFrom($scope.$toObservable('operation.operand'));
+
+    // Create a shared observable from function "saveResult" that is bound to the "Save" button
+    var saveResultObservable = observableChains.saveResultObservableFrom($scope.$createObservableFunction('saveResult'))
+    .share();
+
+    // Create an observable that loads history once an entry has been saved
+    var loadHistoryObservable = observableChains.loadHistoryObservableFrom(saveResultObservable);
 
     // Subscribe to observable stream
     updatePartialObservable.subscribe(function success(response) {
+
         // Replace HTML code with data content
         $scope.operation.resultPartial = response.data;
     }, function failure(data) {
+    
         // Handle error
         console.log("Error loading partial:");
         console.log(data);
     });
 
-    var saveResultObservable = $scope.$createObservableFunction('saveResult')
-    .map(function (operand) {
-        var result = new OperationResult();
-
-        result.operation = operand + "^2";
-        result.value = operand * operand;
-        return result;
-    })
-    .flatMap(function (operationResult) {
-        return operationResult.$save();
-    }).share();
-
-    $scope.alertTimeout = null;
-    saveResultObservable.subscribe(function success(response) {
-        $scope.operation.saveStatus = true;
-            if ($scope.alertTimeout !== null) {
-                $timeout.cancel($scope.alertTimeout);
-            }
-            $scope.alertTimeout = $timeout(function () {
-                $scope.operation.saveStatus = null;
-            }, 1500);
-        }, function failure(data) {
-        $scope.operation.saveStatus = false;
-    });
-
-    var loadHistoryObservable = saveResultObservable.startWith(null).flatMap(function () {
-        var resultHistory = new OperationResult();
-        return resultHistory.$get();
-    });
+    saveResultObservable.subscribe(
+        updateSaveStatus(true),
+        updateSaveStatus(false)
+    );
 
     loadHistoryObservable.subscribe(function success(data) {
         console.log(data);
